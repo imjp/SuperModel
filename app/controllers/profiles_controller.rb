dbdc1b6372ee2b93bcaa060898d7aa33a46ca59f
Jean-Paul Bernadina <jpbernadina@gmail.com>
2011-07-19 04:42:13 -0500

    almost there

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,7 +1,12 @@
 class ProfilesController < ApplicationController
 	def create
 		@user = User.find(params[:user_id])
-		@profile = @user.profiles.create(params[:profile])
+		@profile = @user.create_profile(params[:profile])
 		redirect_to user_path(@user)
+	end
+	
+	def show
+		@user = User.find(params[:user_id])
+		@profile = @user.profile 
 	end
 end
--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,6 @@
 class Profile < ActiveRecord::Base
-  belongs_to :user
+	belongs_to :user
+	
+	attr_accessible :first_name, :last_name, :picture, :sex
+  
 end
