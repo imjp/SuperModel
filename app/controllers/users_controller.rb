695e3d82290fd0519da2b4053baf6c84ba0805c0
Jean-Paul Bernadina <jpbernadina@gmail.com>
2011-07-20 23:23:00 -0500

    profile is not being saved

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   # GET /users/1
   # GET /users/1.json
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:id]) 
 
     respond_to do |format|
       format.html # show.html.erb
@@ -40,7 +40,8 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(params[:user])
+    @user = User.new(params[:user]) 
+	@user.build_profile
 
     respond_to do |format|
       if @user.save
