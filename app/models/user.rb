10b71cfe9af8b1ca7a5b708182418f4ba13a87f4
Jean-Paul Bernadina <jpbernadina@gmail.com>
2011-07-25 03:43:22 -0500

    trying paperclip again

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,17 @@ class User < ActiveRecord::Base
 	has_friendly_id :name, :use_slug => true, :approximate_ascii => true,
 					:reserved_words => ['new', 'users', 'contact', 'about', 'male', 'female', 'portfolios']
 	
-	 
+	
+	path = Rails.root.to_s + '/config/s3.yml'
+	
+	has_attached_file :photo_1,
+					  :styles => {
+					  :thumb=> '100x100#',
+				      :small  => '400x400>' },
+					  :storage => :s3,
+					  :bucket => 'puremodels',
+					  :s3_credentials => path,
+					  :path => '/:style/:id/:filename'
 					  
 					  
 	def create_portfolio
